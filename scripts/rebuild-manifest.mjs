f8dd422787f5cd0c212ad34daeddf38513721c1d
Zylis-Vivian <1625150754@qq.com>
2026-08-19 17:39:55 +0800

    feat: connect dictation and remove textbook center 2026-08-19

--- scripts/rebuild-manifest.mjs
+++ scripts/rebuild-manifest.mjs
@@ -17,7 +17,6 @@ import { PRONUNCIATION } from './src/data/pronunciation'
 import { VOCAB } from './src/data/vocab'
 import { DIALOGUES } from './src/data/dialogue'
 import { GRAMMAR } from './src/data/grammar'
-import { bookVocab } from './src/data/book-vocabulary'
 import { applyPhoneticsIfNeeded } from './src/utils/koreanPhonetics'
 
 const out = new Set()
@@ -34,7 +33,6 @@ PRONUNCIATION.forEach((r) => r.examples.forEach((e) => add(e.ko)))
 VOCAB.forEach((topic) => topic.words.forEach((w) => add(w.korean)))
 DIALOGUES.forEach((d) => { d.lines.forEach((l) => add(l.ko)); ; d.vocabulary.forEach((v) => add(v.word)) })
 GRAMMAR.forEach((g) => g.examples.forEach((e) => add(e.ko)))
-bookVocab.forEach((b) => b.words.forEach((w) => { if (w.korean) add(w.korean); if (w.example) add(w.example) }))
 export const STRINGS = [...out]
 `
 
